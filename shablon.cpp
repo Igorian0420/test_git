00c3a83ac9bc231f2eb3a8eedf99772ccdcc6aa8
gryaznovigor97 <gryaznovigor97@gmail.com>
2019-09-06 13:55:40 +0300

    'test'

---
+++ shablon.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+template <typename T1, typename T2>
+void summ(T1 firstnum, T2 secondnum)
+{
+	std::cout << (firstnum + secondnum);
+}
+
+template <typename T3, typename T4>
+class rawno
+{
+public:
+	rawno(T3 one, T4 two)
+	{
+		if (one > two)
+		{
+			std::cout << "First number bigger then second!" << std::endl;
+		}
+		else if (one < two)
+		{
+			std::cout << "First number smoller then second!" << std::endl;
+		}
+		else
+		{
+			std::cout << "This number the same!" << std::endl;
+		}
+	}
+};
+
+int main()
+{
+	summ(5, 8);
+	std::cout << std::endl;
+
+	rawno<int, int> obj(2, 2);
+
+	return 0;
+}
